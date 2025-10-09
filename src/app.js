a6d9b932b9fdec45cb41a7dc1491cb40294fab6c
Samyaka13 <samyakajmera022@gmail.com>
2025-10-10 03:52:02 +0530

    Added app router in app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-
+import userRouter from './routes/user.routes.js'
 const app = express();
 app.use(cors({
     origin:process.env.CORS_ORIGIN
@@ -8,5 +8,6 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use("/api/v1/users", userRouter)
 
 export default app;
